699bc7ac8f5f589d46485e4d9822cf582091139b
Apurva Deshmukh <aadeshmukh13@gmail.com>
2022-10-18 18:33:43 -0400

    general folder structure

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Apurva Deshmukh on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
