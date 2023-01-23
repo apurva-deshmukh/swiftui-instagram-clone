0ab3da91f28754c5e4f3112affa706b944191afb
Apurva Deshmukh <aadeshmukh13@gmail.com>
2023-01-22 19:00:46 -0500

    added notif ui

---
+++ InstagramClone/View/Notifications/NotificationCell.swift
@@ -0,0 +1,54 @@
+//
+//  NotificationCell.swift
+//  InstagramClone
+//
+//  Created by Apurva Deshmukh on 1/22/23.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    @State private var showPostImage = true
+    var body: some View {
+        HStack {
+            Image("post2")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+            Text("batman")
+                .font(.system(size: 14, weight: .semibold)) +
+            Text(" liked one of your posts")
+                .font(.system(size: 15))
+            
+            Spacer()
+            
+            if showPostImage {
+                Image("post2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+            } else {
+                Button {
+                    
+                } label: {
+                    Text("Follow")
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 8)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                        .font(.system(size: 14, weight: .semibold))
+                }
+
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct NotificationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationCell()
+    }
+}
