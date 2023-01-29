0ca7c1031f567dc7b66a01eea0051fce8e122f9e
Apurva Deshmukh <aadeshmukh13@gmail.com>
2023-01-29 00:47:01 -0500

    create text area

--- InstagramClone/View/UploadPost/UploadPostView.swift
+++ InstagramClone/View/UploadPost/UploadPostView.swift
@@ -42,7 +42,8 @@ struct UploadPostView: View {
                         .frame(width: 96, height: 96)
                         .clipped()
                     
-                    TextField("Enter your caption...", text: $captionText)
+                    TextArea(text: $captionText, placeholder: "Enter your caption...")
+                        .frame(height: 200)
                 }
                 .padding()
                 
